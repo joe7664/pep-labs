ac5ebb1abe03c62b040f2b50afca6cf837895ca1
joe7664 <joe7663@gmail.com>
2022-12-20 19:23:23 +0000

    Saved progress on BooleanOperators 2022-12-20 19:23:23 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,7 +14,10 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        if(b==true)
+            return true;
+        else
+            return false;
     }
 
     /**
@@ -23,7 +26,10 @@ public class Expressions {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        if(b==true)
+            return false;
+        else
+            return true;
     }
 
     /**
@@ -33,7 +39,10 @@ public class Expressions {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        if(b1==true && b2==true)
+            return true;
+        else
+            return false;
     }
 
     /**
@@ -43,7 +52,10 @@ public class Expressions {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        if(b1==true || b2==true)
+            return true;
+        else
+            return false;
     }
 
 
