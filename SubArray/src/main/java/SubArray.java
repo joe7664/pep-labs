ca339ed2ef5454e7c5dba1843067dbf95a1ba033
joe7664 <joe7663@gmail.com>
2023-01-15 06:21:27 +0000

    Saved progress on SubArray 2023-01-15 06:21:27 +0000

--- SubArray/src/main/java/SubArray.java
+++ SubArray/src/main/java/SubArray.java
@@ -13,6 +13,10 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int[] su = new int[end-start];
+        for(int i=start; i<end; i++){
+            su[i-start]=nums[i];
+        }
+        return su;
     }
 }
